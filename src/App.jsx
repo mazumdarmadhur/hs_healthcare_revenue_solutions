42af121c65d6063a18d372595de46f22f03bcb29
Sanjit Majumdar <mazumdarmadhur@gmail.com>
2022-04-16 20:57:25 +0530

    Created the router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { About, Contact, Footer, Homepage, Nav, Services } from './components';
 import './App.scss';
 
 const App = () => {
   return (
     <>
-      <Homepage />
-      <Nav />
-      <About />
-      <Contact />
-      <Services />
-      <Footer />
+      <Routes>
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/' element={<Homepage />} />
+        <Route exact path='/services' element={<Services />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/footer' element={<Footer />} />
+        <Route exact path='/nav' element={<Footer />} />
+        <Route exact path='*' element={<Navigate replace to='/' />} />
+      </Routes>
     </>
   );
 };
