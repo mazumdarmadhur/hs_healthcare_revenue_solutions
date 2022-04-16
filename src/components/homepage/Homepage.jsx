157b5b6bb1b8202ca083a37c9219d65e16a7b11f
Sanjit Majumdar <mazumdarmadhur@gmail.com>
2022-04-17 01:28:59 +0530

    Created the Work Section file

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Homepage.scss'
-import Welcome from '../welcome/Welcome'
+import Welcome from './Welcome'
+import Work from './Work'
 
 const Homepage = () => {
   return (
     <>
       <Welcome />
-
+      <Work />
     </>
   )
 }
---
+++ src/components/homepage/Work.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Work = () => {
+    return (
+        <>
+            <div className="mt-5 work__bg">
+                <div className="py-5">
+                    <div className="text-center work__heading">Here’s what you can expect when you register with HSHRS</div>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="row">
+                                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                                    <h1>
+                                        Medical Billing
+                                    </h1>
+                                    <p className='my-3'>
+                                        Amet minim mollit non deserunt ullamco est sit
+                                        aliqua dolor do amet sint. Velit officia
+                                        consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                                    </p>
+                                    <ul>
+                                        <li>
+                                        Amet minim mollit non deserunt 
+                                        </li>
+                                        <li>
+                                        Amet minim mollit non deserunt 
+                                        </li>
+                                        <li>
+                                        Amet minim mollit non deserunt 
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Work
