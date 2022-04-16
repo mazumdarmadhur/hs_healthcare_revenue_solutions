3f59766ae5cd43f9bb8da95fbb0b7c906a2a502e
Sanjit Majumdar <mazumdarmadhur@gmail.com>
2022-04-17 02:17:13 +0530

    Completed work section

--- src/components/homepage/Work.jsx
+++ src/components/homepage/Work.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import { BsFillCheckCircleFill } from 'react-icons/bs';
+import medical from '../../assets/medical-billing.svg'
+import rcm from '../../assets/rcm.svg'
 
 const Work = () => {
     return (
         <>
-            <div className="my-5 work__bg">
+            <div className="my-5 work__bg d-flex align-items-center">
                 <div className="py-5">
-                    <div className="text-center work__heading">Here’s what you can expect when you register with HSHRS</div>
+                    <div className="text-center work__heading pb-5">Here’s what you can expect when you register with HSHRS</div>
 
+                    {/* TWO COLUMN SECTION STARTS */}
                     <div className="container-fluid mt-2 work__content">
                         <div className="row">
                             <div className="col-10 mx-auto">
+                                {/* FIRST COLUMN STARTS */}
                                 <div className="row">
-                                    <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                                        <h1>
+                                    <div className="col-md-6 pt-5 pt-lg-0 order-2  d-flex justify-content-center flex-column">
+                                        <p className='heading__col'>
                                             Medical Billing
-                                        </h1>
-                                        <p className='my-3'>
+                                        </p>
+                                        <p className='my-3 text__col'>
                                             Amet minim mollit non deserunt ullamco est sit
                                             aliqua dolor do amet sint. Velit officia
                                             consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
                                         </p>
-                                        <div className="d-grid gap-2">
+                                        <div className="d-grid gap-2 text__col">
                                             <div>
                                                 <span className='check'><BsFillCheckCircleFill /> </span>
                                                 Amet minim mollit non deserunt
@@ -36,13 +40,47 @@ const Work = () => {
                                             </div>
                                         </div>
                                     </div>
+                                    <div className="col-lg-6 order-1 header-img">
+                                        <img src={medical} alt="Medical Billing" srcset="" className="img-fluid " />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-lg-6 order-1 order-lg-2 header-img">
-                                <img src="" alt="" srcset="" className="img-fluid "/>
+                                {/* FIRST COLUMN ENDS */}
+
+                                {/* SECOND COLUMN STARTS */}
+                                <div className="row ">
+                                    <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+                                        <p className='heading__col'>
+                                            Revenue Cycle Management
+                                        </p>
+                                        <p className='my-3 text__col'>
+                                            Amet minim mollit non deserunt ullamco est sit
+                                            aliqua dolor do amet sint. Velit officia
+                                            consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+                                        </p>
+                                        <div className="d-grid gap-2 text__col">
+                                            <div>
+                                                <span className='check'><BsFillCheckCircleFill /> </span>
+                                                Amet minim mollit non deserunt
+                                            </div>
+                                            <div>
+                                                <span className='check'><BsFillCheckCircleFill /> </span>
+                                                Amet minim mollit non deserunt
+                                            </div>
+                                            <div>
+                                                <span className='check'><BsFillCheckCircleFill /> </span>
+                                                Amet minim mollit non deserunt
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-6 order-1 order-lg-2 header-img">
+                                        <img src={rcm} alt="Medical Billing" srcset="" className="img-fluid " />
+                                    </div>
+                                </div>
+                                {/* SECOND COLUMN ENDS */}
                             </div>
                         </div>
                     </div>
+                    {/* TWO COLUMN SECTION ENDS */}
                 </div>
             </div>
         </>
